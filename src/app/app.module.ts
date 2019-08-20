d22cbcf3e5868757a02e07ad932e6c48f1c304d7
Anurag Dixit <E01881@hsc.com>
2019-08-20 16:06:23 +0530

    multiple tupe of selectors inline styling and inline html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 // This is type script feature
 // import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
-@NgModule({
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     // This is the feature understood by angular as it is the part of angular module
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core'
+
+// compile to js
+@Component({
+    // meta data for the class to tell angular what to do with the class
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+}) 
+ 
+export class ServerComponent {
+
+}
