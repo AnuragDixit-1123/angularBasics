147f32830d9efd2ee14a94ee85f3a242cfd010ab
Anurag Dixit <E01881@hsc.com>
2019-08-28 17:25:27 +0530

    Observable initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// This is type script feature
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
-@NgModule({ 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    HomeComponent,
+    UserComponent
   ],
   imports: [
-    // This is the feature understood by angular as it is the part of angular module
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
