25ce0300cdf59490a2e8e1cb9b2579dfb920867e
Anurag Dixit <E01881@hsc.com>
2019-08-29 11:13:43 +0530

    Adding the form with the local reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // It is of type HtmlFormElement
+  // onSubmit(form: ElementRef) {
+    // Type chenges after the introduction of ngForm
+  onSubmit(form: NgForm) {
+    console.log('clicked', form)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,16 @@ import { NgModule } from '@angular/core';
 // This is type script feature
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({ 
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+  
   ],
   imports: [
     // This is the feature understood by angular as it is the part of angular module
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
