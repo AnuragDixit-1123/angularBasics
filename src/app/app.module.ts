e35bb304c76ecfbc6d53c108b2609325bd25eb1c
Anurag Dixit <E01881@hsc.com>
2019-08-29 17:33:33 +0530

    Basic commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// This is type script feature
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
-@NgModule({ 
+import { AppComponent } from './app.component'
+import { ShortenPipe } from './shorten.pipe';
+
+@NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    ShortenPipe
   ],
   imports: [
-    // This is the feature understood by angular as it is the part of angular module
     BrowserModule,
     FormsModule
   ],
