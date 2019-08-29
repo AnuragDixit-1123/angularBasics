d9c2acad12e24872d905da234a5d0fa5f5088242
Anurag Dixit <E01881@hsc.com>
2019-08-29 15:47:53 +0530

    Reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// This is type script feature
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
-@NgModule({ 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
-    // This is the feature understood by angular as it is the part of angular module
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
