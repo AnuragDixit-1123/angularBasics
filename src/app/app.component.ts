d42944037831660bebee8c2295335baf1ddfe943
Anurag Dixit <E01881@hsc.com>
2019-08-30 13:04:24 +0530

    initial setup of apiCalls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  loadedPosts: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+
+  }
+
+  onCreatePost(postData: Post) {
+    // // Send Http request
+    this.http
+      .post(
+        'https://angularguide.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+    // .get('https://angularguide.firebaseio.com/posts.json')
+    //   .pipe(map((responseData: {[key: string]: Post}) => {
+      // or use this
+      .get<{ [key: string]: Post}>('https://angularguide.firebaseio.com/posts.json')
+      .pipe(map((responseData) => {
+        const postArray: Post[] = [];
+        for (const data in responseData) {
+          if (responseData.hasOwnProperty(data)) {
+            postArray.push({ ...responseData[data], id: data});
+          }
+        }
+        return postArray;
+      }))
+      .subscribe((posts) => {
+        // console.log(posts);
+        this.loadedPosts = posts;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 // This is type script feature
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({ 
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     // This is the feature understood by angular as it is the part of angular module
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
