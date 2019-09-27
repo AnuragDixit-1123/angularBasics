cab2e7fd57358ecf8ab8b4fc28dff650b5771a39
Anurag Dixit <E01881@hsc.com>
2019-09-27 15:24:34 +0530

    firebase urls added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseApiKey: 'AIzaSyBXM1W0_nQLUaZ9i1F6ksO_-Y1N5PA6-NE'
+
 };
 
 /*
